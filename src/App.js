6010d2aa28f6a71070e5297a7be417c3a9c453a8
Umair Ahmed <soomroumair513@gmail.com>
2023-02-23 01:32:46 +0500

    Third Commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import "./App.css";
 import Navbar from "./componenets/Navbar";
+import TextForm from "./componenets/TextForm";
 
 
 function App() {
   return (
     <>
       <Navbar title="TextUtils" aboutText="About TextUtils"/>
+      <div className="container my-3">
+        <TextForm heading="Enter the Text to analyze below"/>
+      </div>
+      
     </>
   );
 }
